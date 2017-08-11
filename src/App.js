107a22964359ac9528779f8b61b4e62358e01a66
lcicon <ladislav.cicon@wandera.com>
2017-08-11 09:03:22 +0200

    adding cestasnp logo and message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to CestaSNP.sk v4</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-message">
+        Stay tuned for fresh new CestaSNP.sk. In the mean time use <a src="https://cestasnp.sk">CestaSNP.sk v3</a>
+        </div>
       </div>
     );
   }
