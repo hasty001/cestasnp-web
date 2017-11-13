6654d772d368144dd4b763b8999c2d3e21779aa5
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-11-13 22:49:37 +0000

    fix port for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
 
-http.listen(3000, function () {
+http.listen(process.env.PORT || 3000, function () {
   console.log('listening on *:3000')
 })
 
