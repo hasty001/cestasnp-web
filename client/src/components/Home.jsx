1f0107b22c59a6ffe386867f0796d072c7369365
lcicon <ladislav.cicon@wandera.com>
2018-04-16 21:07:29 +0200

    changing home text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div id="Home">
       <p>
-        Na CesteSNP.sk pracujeme dňom a nocou, teda pokiaľ nie sme akurát na horách, v práci, v
+        Hej. Na CesteSNP.sk pracujeme dňom a nocou, teda pokiaľ nie sme akurát na horách, v práci, v
         knižnici, v divadle, u frajerky, či sa hráme s našimi deťmi...
       </p>
       <p>
