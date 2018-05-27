ebb83303b5a859a14742fbb3dcdcb76cd96ace77
Peter Trizuliak <peter.trizuliak@gmail.com>
2018-05-27 21:38:23 +0100

    change kontakt page text

--- client/src/components/Kontakt.jsx
+++ client/src/components/Kontakt.jsx
@@ -3,25 +3,27 @@ import React from 'react';
 const Kontakt = () => {
   return (
     <div id="Kontakt">
-      <p>
-        Ak by si sa k nám chcel pridať a pomocť s vývojom, neváhaj a daj o sebe vedieť. Používame
-        moderné technológie ako React, JavaScript ES6, Node.js a NoSQL databázu. To všetko beží na
-        Heroku cloude a tak máš veľkú šancu sa naučiť čosi nové. Ak to ale už všetko poznáš, môžeš
-        naučiť ty nás ako postaviť CestaSNP.sk na nohy rýchlo a efektívne.
-      </p>
-      <p>
-        Kontakt na nás:{' '}
-        <a target="_blank" href="mailto:info@cestasnp.sk">
-          info@cestasnp.sk
-        </a>.
-      </p>
-      <p>
-        CestuSNP.sk nájdeš na{' '}
-        <a target="_blank" href="https://github.com/hasty001/cestasnp-web">
-          Githube
-        </a>.
-      </p>
-    </div>
+      <p><b>CestaSNP.sk pre vás pripravuje a prevádzkuje:</b></p>
+      <br />
+      <p><b>Občianske združenie CestaSNP.sk</b></p>
+      <p>Gaštanová 4</p>
+      <p>974 01 Banská Bystrica</p>
+      <br />
+      <p>Email: <a target="_blank" href="mailto:info@cestasnp.sk">
+        info@cestasnp.sk
+        </a></p>
+      <br />
+      <p>Všetok vývoj a prevádzku portálu robíme vo svojom voľnom čase. Nakoľko na samotnú každomesačnú prevádzku portálu potrebujeme aj finančné prostriedky, budeme veľmi radi ak našu prácu podporíte aj finančne.</p>
+      <br />
+      <p>Môžete tak urobiť ľubovoľnou čiastkou na účet OZ CestaSNP.sk:</p>
+      <br />
+      <p>Zasielanie zo Slovenska v EUR: SK3483300000002501407836</p>
+      <p>Zasielanie z Česka v CZK: CZ2320100000002501407836</p>
+      <br />
+      <p>Do správy pre príjemcu uveďte prosím svoje meno, ktoré zverejníme v zozname darcov, kde bude uvedené aj na čo boli prostriedky použité.</p>
+      <br />
+      <p>Ďakujeme, že podporujete našu prácu, čo nás posúva vpred.</p>
+    </div >
   );
 };
 
