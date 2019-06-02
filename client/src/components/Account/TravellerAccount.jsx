22666119544e006f73c1dbaa2e7613cb5a2ef32b
Peter Trizuliak <petertrizuliak@Peters-MBP-2.lan>
2019-06-02 15:19:06 +0100

    add success message

--- client/src/components/Account/TravellerAccount.jsx
+++ client/src/components/Account/TravellerAccount.jsx
@@ -7,21 +7,22 @@ class TravellerAccount extends React.Component {
     constructor(props) {
         super(props) 
         this.state = {
-          meno: this.props.traveller.travellerDetails.meno,
-          error: "",
-          popis: this.props.traveller.travellerDetails.text,
-          zaciatok: this.props.traveller.travellerDetails.start_miesto,
-          pocet: this.props.traveller.travellerDetails.number,
-          start_date: this.props.traveller.travellerDetails.start_date,
-          user_id: this.props.traveller.travellerDetails.user_id,
-          loading: 0,
-          edit: {
-              meno: 0,
-              popis: 0,
-              zaciatok: 0,
-              pocet: 0,
-              start_date: 0,
-          }
+            meno: this.props.traveller.travellerDetails.meno,
+            popis: this.props.traveller.travellerDetails.text,
+            zaciatok: this.props.traveller.travellerDetails.start_miesto,
+            pocet: this.props.traveller.travellerDetails.number,
+            start_date: this.props.traveller.travellerDetails.start_date,
+            user_id: this.props.traveller.travellerDetails.user_id,
+            loading: 0,
+            edit: {
+                meno: 0,
+                popis: 0,
+                zaciatok: 0,
+                pocet: 0,
+                start_date: 0,
+            },
+            error: "",
+            successMsg: "",
         }
         this.handleChange=this.handleChange.bind(this)
         this.updateTraveller=this.updateTraveller.bind(this)
@@ -95,7 +96,7 @@ class TravellerAccount extends React.Component {
             return
         }
 
-        if (moment(start_date).diff(moment(), 'days') < 0) {
+        if (start_date !== this.props.traveller.travellerDetails.start_date && moment(start_date).diff(moment(), 'days') < 0) {
             this.setState({
                 error: "Začiatok cesty je v minulosti. Vyber iný dátum!"
             })
@@ -128,7 +129,15 @@ class TravellerAccount extends React.Component {
         .then(travellerDetails => {
             console.log('UPDATED successfully ', travellerDetails);
             this.setState({
-                loading: 0
+                loading: 0,
+                edit: {
+                    meno: 0,
+                    popis: 0,
+                    zaciatok: 0,
+                    pocet: 0,
+                    start_date: 0,
+                },
+                successMsg: "Detaily tvojej cesty sme úspešne zmenili"
             })
             this.props.traveller.updateTravellerDetails({
                 meno,
@@ -162,12 +171,12 @@ class TravellerAccount extends React.Component {
             }}>
                 <h2>Moja cesta</h2>
                 <p>Tu si môžeš upraviť detaily o svojej ceste a zároveň posielať správy.</p>
-                <label htmlFor="nazov">
+                <label htmlFor="meno">
                     <span onClick={() => this.triggerEdit("meno") }>Moja cesta <i className="fas fa-edit" ></i></span>
                     {this.state.edit.meno ? 
                         <input
-                            id="nazov"
-                            name="nazov"
+                            id="meno"
+                            name="meno"
                             value={this.state.meno}
                             type="text"
                             onBlur={(e) => {
@@ -194,7 +203,7 @@ class TravellerAccount extends React.Component {
                             onChange={this.handleChange}
                         />
                         :
-                        <p className="travellerP">{this.state.meno}</p>
+                        <p className="travellerP">{this.state.popis}</p>
                     }
                 </label>
                 <label htmlFor="zaciatok">
@@ -253,6 +262,7 @@ class TravellerAccount extends React.Component {
                     }
                 </label>
                 {this.state.error && <p className="errorMsg">{this.state.error}</p>}
+                {this.state.successMsg && <p className="successMsg">{this.state.successMsg}</p>}
                 {this.state.loading ? 
                     <Loader />
                     :
