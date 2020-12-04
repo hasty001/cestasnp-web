4b59ff28c19450419bfa20e49684de610cd31627
tom-gregorovic <gregorovic.tomas@gmail.com>
2020-12-04 15:17:41 +0100

    footer mobile

--- client/src/components/SiteFooter.jsx
+++ client/src/components/SiteFooter.jsx
@@ -9,39 +9,49 @@ const SiteFooter = (props) => {
   
   return (
     <div className="site-footer no-print">
-      <div className="col1">
-        <A href="/ucet/pridatpoi" className="red-button x2" >Registruj sa</A>
+      <div className="row1">
+        <div className="col1">
+          <A href="/ucet/pridatpoi" className="red-button x2" >Registruj sa</A>
+        </div>
+        <div className="col2">
+          <A href="/pred/articles/article/10003" className="red-button x2" >Daruj 2%</A>
+        </div>
+        <div className="col3">
+          <A href="/pred/articles/article/10003" className="red-button x2" >Podpor nás</A>
+        </div>
       </div>
-      <div className="col2">
-        <A href="/pred/articles/article/10003" className="red-button x2" >Daruj 2%</A>
-
-        <A href="/"><img src={logoFooter} className="logo-footer"/></A>
-        <A className="footer-link" href="/kontakt">O nás</A>
-        <A className="footer-link" href="/pred/articles/1">Články</A>
-        <A className="footer-link" href="/pred/pois">Dôležité miesta</A>
-        <A className="footer-link" href="/pred/itinerar">Itinerár</A>
-        <A className="footer-link" href={settingsData.activeLink.href}>LIVE sledovanie</A>
-        <A className="footer-link" href="/na/archive">Archív</A>
-        <A className="footer-link" href="/pred/articles/article/379">Zoznam darcov</A>
-      </div>
-      <div className="col3">
-        <A href="/pred/articles/article/10003" className="red-button x2" >Podpor nás</A>
-        <h2>Kontakt</h2>
-        <p>
-          Občianske združenie<br/>
-          CestaSNP.sk<br/>
-          <br/>
-          Gaštanová 4<br/>
-          974 01 Banská Bystrica<br/>
-          <br/>
-          info@cestasnp.sk
-        </p>
-        <p>
-          <br/>
-          Ďakujeme,<br/>
-          že podporujete našu prácu,<br/>
-          čo nás posúva vpred.
-        </p>
+      <div>
+        <div className="col1">
+          <h2>Prečo sa registrovať</h2>
+        </div>
+        <div className="col2">
+          <A href="/"><img src={logoFooter} className="logo-footer"/></A>
+          <A className="footer-link" href="/kontakt">O nás</A>
+          <A className="footer-link" href="/pred/articles/1">Články</A>
+          <A className="footer-link" href="/pred/pois">Dôležité miesta</A>
+          <A className="footer-link" href="/pred/itinerar">Itinerár</A>
+          <A className="footer-link" href={settingsData.activeLink.href}>LIVE sledovanie</A>
+          <A className="footer-link" href="/na/archive">Archív</A>
+          <A className="footer-link" href="/pred/articles/article/379">Zoznam darcov</A>
+        </div>
+        <div className="col3">
+          <h2>Kontakt</h2>
+          <p>
+            Občianske združenie<br/>
+            CestaSNP.sk<br/>
+            <br/>
+            Gaštanová 4<br/>
+            974 01 Banská Bystrica<br/>
+            <br/>
+            info@cestasnp.sk
+          </p>
+          <p>
+            <br/>
+            Ďakujeme,<br/>
+            že podporujete našu prácu,<br/>
+            čo nás posúva vpred.
+          </p>
+        </div>
       </div>
     </div>
   )
