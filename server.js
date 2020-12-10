2e86618fed89a61a762317a1bbc4457f21da9077
tom-gregorovic <gregorovic.tomas@gmail.com>
2020-12-10 16:01:09 +0100

    response compression support

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const { getMeta } = require('./server/meta');
 const { MongoClient } = require('mongodb');
+const compression = require('compression');
 
 const app = express();
 const http = require('http').Server(app);
@@ -25,6 +26,21 @@ if (process.env.PORT) {
     }
   });
 }
+
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // Will not compress responses, if this header is present
+    return false;
+  }
+  // Resort to standard compression
+  return compression.filter(req, res);
+};
+
+// Compress all HTTP responses
+app.use(compression({
+  filter: shouldCompress,
+  threshold: 1024
+}));
 
 app.use(express.static(root));
 app.use(bodyParser.urlencoded({ extended: true }));
