150801c1f285db3343e63a347837faa1367301a7
tom-gregorovic <gregorovic.tomas@gmail.com>
2021-03-04 15:08:05 +0100

    restore icon

--- client/src/components/reusable/CanMaximize.jsx
+++ client/src/components/reusable/CanMaximize.jsx
@@ -9,7 +9,7 @@ const CanMaximize = ({ children }) => {
     <div className="can-maximize">
       <a className="action" href="#" onClick={e => { e.preventDefault(); e.currentTarget.parentElement.classList.toggle("maximized"); }}>
         <i className="fas fa-external-link-alt not-maximized" title="maximalizovať"></i>
-        <i className="fa fa-times maximized" title="obnoviť"></i>
+        <i className="far fa-window-restore maximized" title="obnoviť"></i>
       </a>
       {children}
     </div>);
